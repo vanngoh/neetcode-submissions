dd3234e15672b09bf6509ea29e64fbaef03c293d
Vann <125730430+vanngoh@users.noreply.github.com>
2026-06-23 17:21:11 +0900

    Add: top-k-elements-in-list - submission-4

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-4.py
@@ -0,0 +1,19 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        ref = {}
+        for num in nums:
+            if ref.get(num, None) is None:
+                ref[num] = 1
+                continue
+            ref[num] += 1
+
+        # Get the first nth keys with the largest value
+        res = heapq.nlargest(k, ref, key=ref.get)
+
+        ## Sort the dict into list of tuple
+        # desc_ref = sorted(ref.items(), key=lambda item: item[1], reverse=True)
+        # res = []
+        # for i in range(k):
+        #     res.append(desc_ref[i][0])
+
+        return res
